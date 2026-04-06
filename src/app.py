80e30da6fd5eeb1ce9ab05567617dd20efca06f3
Arturo Fagundo <arturo.fagundo@gmail.com>
2026-04-06 07:37:01 -0400

    Add new fund details

--- src/app.py
+++ src/app.py
@@ -1,17 +1,14 @@
 import streamlit as st
-import polars as pl
-import pandas as pd
-import os
-import json
-from data_utils import load_mappings, save_mappings
 
 # Page Configuration
 st.set_page_config(page_title="Portfolio Manager", layout="wide", page_icon="💰")
 
 # --- ROUTING ---
 
-pg = st.navigation([
-    st.Page("pages/holdings.py", title="Portfolio Holdings", icon="💰"),
-    st.Page("pages/mappings.py", title="Fund Data Management", icon="🏗️"),
-])
+pg = st.navigation(
+    [
+        st.Page("pages/holdings.py", title="Portfolio Holdings", icon="💰"),
+        st.Page("pages/fund_details.py", title="Fund Details Management", icon="🏗️"),
+    ]
+)
 pg.run()
